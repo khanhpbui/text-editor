1cdc9559370559a49c1a1527965b8a933c9b6ef7
Khanh Bui <pkkhanhbui@gmail.com>
2023-01-23 11:08:23 -0500

    add workbox pluggins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,35 @@ module.exports = () => {
     },
     // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
      
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+       // Creates a manifest.json file.
+       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
     
 // TODO: Add CSS loaders and babel to webpack.
